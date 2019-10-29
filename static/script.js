d8061fc6d4ec47a2802edb9a989108a7323c00b2
regi18 <31547194+regi18@users.noreply.github.com>
2019-10-29 22:43:16 +0100

    Improved script

--- static/script.js
+++ static/script.js
@@ -1,38 +1,56 @@
 data = [
-    ["https://github.com/regi18/Sonoff-Blynk", "ARDUINO", "&emsp;/ SONOFF BLYNK"],
-    ["https://github.com/regi18/Espegi", "ARDUINO", "&emsp;/ ESPEGI"],
-    ["https://github.com/regi18/offset-chiper", "CRYPTO", "&emsp;&emsp;/ OFFSET CHIPER"],
-    ["https://github.com/regi18/hexascii2bin", "&emsp;&emsp;CTF", "&emsp;&emsp;&emsp;/ HEXASCII2BIN"],
-    ["https://github.com/regi18/plotMouseMovements", "&emsp;&emsp;CTF", "&emsp;&emsp;&emsp;/ PLOT MOUSE MOVEMENTS"],
-    ["https://github.com/regi18/wpstool", "EXPLOIT", " / WPS TOOL"],
-    ["https://github.com/regi18/vuejs-generate-component", "SCRIPTS", " / VUE.JS GENERATE COMPONENT"],
+    ["https://github.com/regi18/Sonoff-Blynk", "ARDUINO", "SONOFF BLYNK"],
+    ["https://github.com/regi18/Espegi", "ARDUINO", "ESPEGI"],
+    ["https://github.com/regi18/offset-chiper", "CRYPTO", "OFFSET CHIPER"],
+    ["https://github.com/regi18/hexascii2bin", "CTF", "HEXASCII2BIN"],
+    ["https://github.com/regi18/plotMouseMovements", "CTF", "PLOT MOUSE MOVEMENTS"],
+    ["https://github.com/regi18/wpstool", "EXPLOIT", "WPS TOOL"],
+    ["https://github.com/regi18/vuejs-generate-component", "SCRIPTS", "VUE.JS GENERATE COMPONENT"],
 ]
 
-
-
-function makeAList(array) {
-    for (let i of array) {
-        $("#contentList").append(`<li>&emsp;<a target="_blank" href=${i[0]}>${i[1]}${i[2]}</a></li>`);
-    }
-}
+donateList = [
+    ["BTC", "15cahY8GCj3at48vuPSDhQkBkyL51JXr9C"],
+    ["LTC", "LXs2Z6UYsfxR2Q9hfvfrdDxdowoX4wWVie"],
+    ["ETH", "0xb006536907DceD1723a9c4EaEb7D3905fBfA088a"]
+]
 
 function filterList(filter) {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    ul = document.getElementById("contentList");
-    li = ul.getElementsByTagName('li');
+    let node = document.querySelector("#contentList");
+    node.innerHTML = '';
 
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+    for (let i of data) {
+        if (i[1] === filter || filter === "") {
+            var html = `<li class="post-teaser"><a target="_blank" href=${i[0]}>
+                <span class="post-teaser__title">${i[2]}</span>
+                <span class="post-teaser__date">${i[1]}</span>
+                </a></li>
+            `;
+
+            node.insertAdjacentHTML('beforeend', html);
         }
     }
+
+    return false;
 }
 
 
-makeAList(data);
+function showDonate() {
+    let node = document.querySelector("#contentList");
+    node.innerHTML = '';
+
+    for (let i of donateList) {
+        var html =
+        `
+        <li class="post-teaser donate">
+        <span class="post-teaser__title">${i[1]}</span>
+        <span class="post-teaser__date">${i[0]}</span>
+        </li>
+        `;
+
+        node.insertAdjacentHTML('beforeend', html);
+    }
+
+    return false;
+}
+
+document.addEventListener("load", filterList(""));
